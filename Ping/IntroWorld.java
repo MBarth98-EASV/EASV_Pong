003e43338aa498429cc5a1b8645e0be39134e274
Mads Mandahl-Barth <mads-barth@live.dk>
2021-09-17 12:34:41 +0200

    Update IntroWorld.java

--- Ping/IntroWorld.java
+++ Ping/IntroWorld.java
@@ -1,7 +1,7 @@
 import greenfoot.*;
 
 /**
- * Write a description of class IntroWorld here.
+ * Write a description of class IntroWorld here
  * 
  * @author (your name) 
  * @version (a version number or a date)

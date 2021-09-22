0e5be5a25da6981b0935217595f9d923fa0e5d32
sandbxk <90904706+sandbxk@users.noreply.github.com>
2021-09-22 15:47:33 +0200

    ScoreCounterGlow doesn't need to inherit from ScoreCounter

--- Ping/ScoreCounterGlow.java
+++ Ping/ScoreCounterGlow.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class ScoreCounterGlow extends ScoreCounter
+public class ScoreCounterGlow extends Effects
 {
     public ScoreCounterGlow()
     {
